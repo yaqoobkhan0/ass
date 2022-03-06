3d5ad05084dc1e115a1ae45ad6a2f78dd9dc738b
yaqoobkhan0 <99115980+yaqoobkhan0@users.noreply.github.com>
2022-03-06 19:43:14 +0530

    Add files via upload

---
+++ test4/index.js
@@ -0,0 +1,14 @@
+function iphone(){
+    var a = document.getElementById("iphone")
+    a.classList.add("one")
+}
+
+function mi(){
+    var a = document.getElementById("mi")
+    a.classList.add("two")  
+}
+
+function vivo(){
+    var a = document.getElementById("vivo")
+    a.classList.add("three")  
+}
