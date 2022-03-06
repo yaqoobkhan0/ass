fad6d5a880b5807780fa23d07dad7870b110aaaa
yaqoobkhan0 <99115980+yaqoobkhan0@users.noreply.github.com>
2022-03-06 15:23:01 +0530

    Add files via upload

---
+++ 3march/index1.js
@@ -0,0 +1,14 @@
+function swap() {
+    var a = 10 ;
+    var b = 20 ;
+    [a,b]=[b,a];
+    // console.log("a:",a);
+    // console.log("b:",b);
+
+
+alert("value of a="+a+"and value of b="+b);
+
+
+
+}
+   
